de19122ec9d2435e7b062b6582b3211487686b9f
peter-njuku <njuku2005@gmail.com>
2026-06-10 22:28:49 +0300

    Serving other file types in assets

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	//"image"
+	"log"
 	"net/http"
 )
 
 func main() {
+	port := "8080"
 	serveMux := http.NewServeMux()
-	server := http.Server{
-		Addr:    ":8080",
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
+
+	serveMux.Handle("/pages", http.FileServer(http.Dir(".")))
+
 	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	log.Printf("Serving files from root on port %s\n", port)
 	err := server.ListenAndServe()
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 }
